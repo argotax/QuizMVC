e8f09449325864d5998080afd50d211302c8e53d
argotax <maxoble.dl@gmail.com>
2018-12-17 15:42:20 +0100

    'réorga_tree'

---
+++ public/javascript/index.js
@@ -0,0 +1,35 @@
+var socket = io.connect('http://10.2.4.26:8080');
+
+$("#signinForm").submit(function(e) {
+  e.preventDefault();
+});
+$("#signupForm").submit(function(e) {
+  e.preventDefault();
+});
+
+$("#country").countrySelect({
+  preferredCountries: ['fr']
+});
+
+$('.modal').on('shown.bs.modal', function() {
+  $(this).find('[autofocus]').focus();
+});
+
+function signin(){
+  socket.emit('signin', $('#connect-login').val(), $('#connect-password').val());
+}
+function signup(){
+  socket.emit('signup', $('#login').val(), $('#password').val(), $('#confpassword').val(), $('#mail').val(), $('#confmail').val(), $('#country').val());
+}
+
+socket.on('redirect', function(destination) {
+  window.location.href = destination;
+});
+socket.on('connection_error', function(error) {
+  $('#connectError').css({display:'block'});
+  $('#connectError').html(error);
+});
+socket.on('inscription_error', function(error) {
+  $('#inscriptionError').css({display:'block'});
+  $('#inscriptionError').html(error);
+});
