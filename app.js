cfdadfe03cbfbad332e125c5917599961a89eed5
argotax <maxoble.dl@gmail.com>
2018-12-17 16:43:03 +0100

    'integration_passport'

--- app.js
+++ app.js
@@ -3,22 +3,38 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+let sess={
+  maxAge:86400,
+  secret: 'tata',
+  name: 'express_session-cookie',
+  proxy: true,
+  resave: true,
+  saveUninitialized: true
+};
 
 var indexRouter = require('./routes/index');
-
+const usersRouter = require('./routes/users');
+const loginRouter = require('./routes/login');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
-
+const passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session(sess));
+app.use(session(sess));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/users/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
