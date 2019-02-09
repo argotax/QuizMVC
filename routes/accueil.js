2f09c080c9d4152bb809833a0dbc6970f8d734ad
Pierre MOUCHEL <pierre.mouchel@imie.fr>
2019-02-09 16:24:05 +0100

    'join_query'

--- routes/accueil.js
+++ routes/accueil.js
@@ -5,41 +5,24 @@ const Sequelize = require('sequelize');
 
 const Op = Sequelize.Op;
 
+models.broquiz_user.hasMany(models.broquiz_friend, {foreignKey : 'friend_p1'});
+models.broquiz_friend.belongsTo(models.broquiz_user, {foreignKey : 'friend_p1'});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
   var friends = [];
 
-  models
-  .broquiz_friend
-  .findAll({
-    where:{ [Op.or]: [{friend_p1: req.session.user_id}, {friend_p2: req.session.user_id}], friend_status: 5 },
-    attributes: ['friend_p1', 'friend_p2']
+  models.broquiz_friend.findAll({
+    where:{ [Op.or]: [{friend_p1: req.session.user_id}, {friend_p2: req.session.user_id}] },
+    include: [ { model: models.broquiz_user, as: 'broquiz_user' } ]
   }).then(
     user => {
+      friends = [];
       user.forEach(function(element) {
-        if (element.friend_p1 != req.session.user_id) {
-          friends.push(element.friend_p1);
-        } else if (element.friend_p2 != req.session.user_id) {
-          friends.push(element.friend_p2);
-        }
+        friends.push(element.broquiz_user.user_login);
       });
-      models
-      .broquiz_user
-      .findAll({
-        where:{ user_id: friends },
-        attributes: ['user_login']
-      }).then(
-        friend => {
-          friends = [];
-          friend.forEach(function(element) {
-            friends.push(element.user_login);
-          });
-          res.render('accueil',{id: req.session.user_id, login: req.session.user_login, friends: friends})
-        }
-      ).catch(function (err) {
-        console.log('Error query !');
-      });
+      res.render('accueil',{id: req.session.user_id, login: req.session.user_login, friends: friends})
     }
   ).catch(function (err) {
     console.log('Error query !');
