877853b3eb41eccf220048a4cf85ab1c96435531
pierre <mouchelp72531@gmail.com>
2019-02-11 15:49:46 +0100

    'correction_ami'

--- routes/accueil.js
+++ routes/accueil.js
@@ -2,47 +2,66 @@ var express = require('express');
 var router = express.Router();
 const models = require('../models');
 const Sequelize = require('sequelize');
+const sequelize = new Sequelize('broquiz', 'root', 'root', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
 
 const Op = Sequelize.Op;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-  var friends = [];
   var compteur = 0;
+  var friends = [];
+  var friends_id = [];
 
   models.broquiz_friend.findAll({
-    attributes: ['friend_id', 'friend_status', [models.sequelize.literal('CASE WHEN friend_p1 = 1 THEN friend_p2 ELSE friend_p1 END'), 'friend_p1']],
-    where:{ [Op.or]: [{friend_p1: req.session.user_id}, {friend_p2: req.session.user_id}] }
+    attributes: ['friend_id', 'friend_p1', 'friend_status'],
+    where:{ friend_p2: req.session.user_id }
   }).then(
     user => {
       user.forEach(function(element) {
-        friends.push(element.friend_p1);
+        friends.push([element.friend_id, element.friend_p1, element.friend_status]);
       });
-      models.broquiz_user.findAll({
-        attributes: ['user_login'],
-        where:{ user_id:{[Op.or]: friends}}
+      models.broquiz_friend.findAll({
+        attributes: ['friend_id', 'friend_p2', 'friend_status'],
+        where:{ friend_p1: req.session.user_id, friend_status: 5}
       }).then(
         friend => {
-          friends = [];
           friend.forEach(function(element) {
-            friends.push([user[compteur].friend_id, element.user_login, user[compteur].friend_status]);
-              compteur = compteur + 1;
+            friends.push([element.friend_id, element.friend_p2, element.friend_status]);
           });
-          res.render('accueil',{id: req.session.user_id, login: req.session.user_login, friends: friends})
+          friends.forEach(function(element){
+            friends_id.push(element[1]);
+          });
+          models.broquiz_user.findAll({
+            attributes: ['user_id', 'user_login'],
+            where:{user_id:{[Op.in]: friends_id}},
+            order: sequelize.fn('FIELD', sequelize.col('user_id'), '3', '5', '30', '27', '28')
+          }).then(
+            login => {
+              login.forEach(function(element) {
+                friends[compteur].push(element.user_login);
+                compteur += 1;
+              });
+              console.log(friends);
+              res.render('accueil',{id: req.session.user_id, login: req.session.user_login, friends: friends})
+            }
+          ).catch(function (err) {
+            console.log('Error query !');
+            res.render('accueil',{id: req.session.user_id, login: req.session.user_login})
+          });
         }
       ).catch(function (err) {
         console.log('Error query !');
-        console.log(req.session.user_login);
         res.render('accueil',{id: req.session.user_id, login: req.session.user_login})
       });
     }
   ).catch(function (err) {
     console.log('Error query !');
-    console.log(req.session.user_login);
     res.render('accueil',{id: req.session.user_id, login: req.session.user_login})
   });
-
 });
 
 module.exports = router;
