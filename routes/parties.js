f9be79d8df8715aeda2eff8a2449c220b5f9baf2
pierre <mouchelp72531@gmail.com>
2019-02-18 16:45:21 +0100

    'design'

--- routes/parties.js
+++ routes/parties.js
@@ -2,9 +2,14 @@ var express = require('express');
 var router = express.Router();
 const models = require('../models');
 const Sequelize = require('sequelize');
+var app = express();
 
 router.get('/', function(req, res, next) {
   res.render('parties',{id: req.session.user_id, login: req.session.user_login, friends: req.session.friends})
+});
+
+router.get('/partie/:numero', function(req, res, next) {
+  res.render('partie')
 });
 
 module.exports = router;
