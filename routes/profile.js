f4d63b89e2bc527cf2df19a30bbd9dcd3e8c64ba
argotax <maxime.oble@imie.fr>
2019-04-30 15:26:57 +0200

    ajout info profil et calculs info profil

--- routes/profile.js
+++ routes/profile.js
@@ -9,14 +9,14 @@ const sequelize = new Sequelize('broquiz', 'root', 'root', {
   dialect: 'mysql'
 });
 
-var renderPage = function(req, res, values) {
-  console.log('redirect');
-  console.log(values);
+var renderPage = function(req, res, values, ansTab, nbWins, winRate) {
   res.render('profile', {
     id: req.session.user_id,
     login: req.session.user_login,
-    rang: values[0],
-    winLose: values[1]
+    rangTab: values,
+    winLose: ansTab,
+    nbWins: nbWins,
+    winRate: winRate
   });
 };
 
@@ -43,6 +43,10 @@ router.get('/', function(req, res, next) {
   var wrongAns = 0;
   var rightAns = 0;
   var noAns = 0;
+  var wins = 0;
+  var loses = 0;
+  var nulls = 0;
+  var winRate= 0;
 
   var rankQuery = new Promise(function(resolve, reject) {
     models.broquiz_user.findAll({
@@ -54,7 +58,7 @@ router.get('/', function(req, res, next) {
       users => {
         var count = 1;
         users.forEach(function(user) {
-          if (user.user_id == 4) { //a ramplacer par l'id qu'on cherche
+          if (user.user_id == 1) { //a ramplacer par l'id qu'on cherche
             rang = count;
             resolve(rang);
           }
@@ -69,11 +73,37 @@ router.get('/', function(req, res, next) {
       })
     });
   }); //end promise rankquery
-  rankQuery.then(function() {
-    console.log(rang);
-  });
 
-  var winLoseQuery = new Promise(function(resolve, reject) {
+  var countryRankQuery = new Promise(function(resolve, reject) {
+    models.broquiz_user.findAll({
+      attributes: ['user_login', 'user_points', 'user_id', 'user_country'],
+      order: [
+        ['user_points', 'DESC']
+      ],
+      where: {
+        user_country: 'Azerbaijan'
+      }
+    }).then(
+      users => {
+        var count = 1;
+        users.forEach(function(user) {
+          if (user.user_id == 1) { //a ramplacer par l'id qu'on cherche
+            countryRang = count;
+            resolve(countryRang);
+          }
+          count++;
+        });
+      }
+    ).catch(function(err) {
+      console.log('Error query !', err);
+      res.render('accueil', {
+        id: req.session.user_id,
+        login: req.session.user_login
+      })
+    });
+  }); //end promise countryRankquery
+
+  var winLoseQuery_p1 = new Promise(function(resolve, reject) {
     models.broquiz_round.findAll({
       attributes: ['round_q1_p1', 'round_q2_p1', 'round_q3_p1'],
 
@@ -96,18 +126,115 @@ router.get('/', function(req, res, next) {
           } else if (result[i].round_q1_p1 == 14) {
             noAns++;
           }
-        }//end for
-        var valuesTab = [rightAns, wrongAns, noAns];
-        console.log('V : ', rightAns, 'W : ', wrongAns, 'N : ', noAns);
-        resolve(valuesTab);
+        } //end for
+
+        resolve();
       }).catch(function(err) {
       console.log('Error query !', err);
     });
   });
 
-  Promise.all([rankQuery, winLoseQuery]).then(function(values) {
-    console.log(values[1]);
-    renderPage(req, res, values);
+  var winLoseQuery_p2 = new Promise(function(resolve, reject) {
+    models.broquiz_round.findAll({
+      attributes: ['round_q1_p2', 'round_q2_p2', 'round_q3_p2'],
+
+      include: [{
+          model: models.broquiz_game,
+          as: 'game',
+          where: {
+            game_p2: 1
+          }
+        } //a ramplacer avec l'id du mec qu'on cherche
+      ]
+
+    }).then(
+      result => {
+        for (var i = 0; i < result.length; i++) {
+          if (result[i].round_q1_p1 == 7) {
+            rightAns++;
+          } else if (result[i].round_q1_p1 == 8) {
+            wrongAns++;
+          } else if (result[i].round_q1_p1 == 14) {
+            noAns++;
+          }
+        } //end for
+
+        resolve();
+      }).catch(function(err) {
+      console.log('Error query !', err);
+    });
+  });
+
+  var numberWinLoseQuery_p1 = new Promise(function(resolve, reject) {
+    models.broquiz_game.findAll({
+      attributes: ['game_p1', 'game_p1_score', 'game_p2_score', 'game_status'],
+      where: {
+        game_p1: 1,
+        game_status: 13
+      }
+    })
+    .then(
+      games => {
+        var count = 1;
+        games.forEach(function(game) {
+          console.log('one game p1');
+          if (game.game_p1_score>game.game_p2_score) {
+            console.log('win p1');
+            wins++;
+          }
+          else if (game.game_p1_score<game.game_p2_score) {
+            console.log('lose p1');
+            loses++;
+          }
+          else if (game.game_p1_score===game.game_p2_score) {
+            console.log('null p1');
+            nulls++;
+          }
+        });
+        resolve();
+      }).catch(function(err) {
+      console.log('Error query !', err);
+    });
+  });
+
+  var numberWinLoseQuery_p2 = new Promise(function(resolve, reject) {
+    models.broquiz_game.findAll({
+      attributes: ['game_p2', 'game_p2_score', 'game_p1_score', 'game_status'],
+      where: {
+        game_p2: 1,
+        game_status: 13
+      }
+    })
+    .then(
+      games => {
+        var count = 1;
+        games.forEach(function(game) {
+          console.log('one game p2');
+          if (game.game_p2_score>game.game_p1_score) {
+            console.log('win p2');
+            wins++;
+          }
+          else if (game.game_p2_score<game.game_p1_score) {
+            console.log('lose p2');
+            loses++;
+          }
+          else if (game.game_p1_score===game.game_p2_score) {
+            console.log('null p2');
+            nulls++;
+          }
+        });
+        resolve();
+      }).catch(function(err) {
+      console.log('Error query !', err);
+    });
+  });
+
+  Promise.all([rankQuery, countryRankQuery, numberWinLoseQuery_p1, numberWinLoseQuery_p2, winLoseQuery_p1, winLoseQuery_p2]).then(function(values) {
+    var ansTab = [rightAns, wrongAns, noAns];
+    var nbWins = [wins, loses, nulls];
+    winRate = parseFloat(wins*100/(wins+loses)).toFixed(2);
+    console.log('winrate : ', winRate);
+    renderPage(req, res, values, ansTab, nbWins, winRate);
   });
 
 }); //end router.get /
