351d91be1582b377381d7745a808c3ca95ae40d6
argotax <maxime.oble@imie.fr>
2019-04-30 11:02:52 +0200

    multiple querry with promises

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const promise = require('promise');
+var Promise = require('promise');
 const models = require('../models');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
@@ -8,10 +8,31 @@ const sequelize = new Sequelize('broquiz', 'root', 'root', {
   host: 'localhost',
   dialect: 'mysql'
 });
-models.broquiz_round.hasMany(models.broquiz_game, {as: 'joueur_1', foreignKey : 'game_p1'});
-models.broquiz_game.belongsTo(models.broquiz_round, {as: 'joueur_1', foreignKey : 'game_p1'});
-models.broquiz_game.hasMany(models.broquiz_round, {as: 'game', foreignKey : 'round_game'});
-models.broquiz_round.belongsTo(models.broquiz_game, {as: 'game', foreignKey : 'round_game'});
+
+var renderPage = function(req, res) {
+  console.log('redirect');
+  res.render('profile', {
+    id: req.session.user_id,
+    login: req.session.user_login
+  });
+};
+
+models.broquiz_round.hasMany(models.broquiz_game, {
+  as: 'joueur_1',
+  foreignKey: 'game_p1'
+});
+models.broquiz_game.belongsTo(models.broquiz_round, {
+  as: 'joueur_1',
+  foreignKey: 'game_p1'
+});
+models.broquiz_game.hasMany(models.broquiz_round, {
+  as: 'game',
+  foreignKey: 'round_game'
+});
+models.broquiz_round.belongsTo(models.broquiz_game, {
+  as: 'game',
+  foreignKey: 'round_game'
+});
 
 var app = express();
 router.get('/', function(req, res, next) {
@@ -20,61 +41,79 @@ router.get('/', function(req, res, next) {
   var rightAns = 0;
   var noAns = 0;
 
-  models.broquiz_user.findAll({
-    attributes: ['user_login','user_points','user_id'],
-    order: [
-      ['user_points', 'DESC']
-    ]
-  }).then(
-    users => {
-      var count = 1;
-      users.forEach(function(user) {
-        if (user.user_id == 4) {//a ramplacer par l'id qu'on cherche
-          rang = count;
-        }
-        count++;
-      });
-    }
-  ).catch(function (err) {
-    console.log('Error query !', err);
-    res.render('accueil',{id: req.session.user_id, login: req.session.user_login})
+  var rankQuery = new Promise(function(resolve, reject) {
+    models.broquiz_user.findAll({
+      attributes: ['user_login', 'user_points', 'user_id'],
+      order: [
+        ['user_points', 'DESC']
+      ]
+    }).then(
+      users => {
+        var count = 1;
+        users.forEach(function(user) {
+          if (user.user_id == 4) { //a ramplacer par l'id qu'on cherche
+            rang = count;
+            resolve();
+          }
+          count++;
+        });
+      }
+    ).catch(function(err) {
+      console.log('Error query !', err);
+      res.render('accueil', {
+        id: req.session.user_id,
+        login: req.session.user_login
+      })
+    });
+  }); //end promise rankquery
+  rankQuery.then(function() {
+    console.log(rang);
   });
 
+  var winLoseQuery = new Promise(function(resolve, reject) {
+    models.broquiz_round.findAll({
+      attributes: ['round_q1_p1', 'round_q2_p1', 'round_q3_p1'],
 
+      include: [{
+          model: models.broquiz_game,
+          as: 'game',
+          where: {
+            game_p1: 1
+          }
+        } //a ramplacer avec l'id du mec qu'on cherche
+      ]
 
-  models.broquiz_round.findAll({
-    attributes: ['round_q1_p1','round_q2_p1','round_q3_p1'],
+    }).then(
+      result => {
+        for (var i = 0; i < result.length; i++) {
+          if (result[i].round_q1_p1 == 7) {
+            rightAns++;
+          } else if (result[i].round_q1_p1 == 8) {
+            wrongAns++;
+          } else if (result[i].round_q1_p1 == 14) {
+            noAns++;
+          }
+        }
+        console.log('V : ', rightAns, 'W : ', wrongAns, 'N : ', noAns);
+        resolve();
+      }).catch(function(err) {
+      console.log('Error query !', err);
+    });
+  });
 
-    include: [
-      { model: models.broquiz_game, as: 'game', where:{game_p1: 1}}
-    ]
+  Promise.all([rankQuery, winLoseQuery]).then(function(values) {
+    console.log('queries donr');
+    renderPage(req, res);
+  });
 
-  }).then(
-    result=>{
-      result.forEach(function(element){
-        console.log(element.round_q1_p1);
-      });
-
-
-      // for (var i = 0; i < result.length; i++) {
-      //   if (result[i].round_q1_p1 == 7) {
-      //     rightAns++;
-      //   }
-      //   else if (result[i].round_q1_p1 == 8) {
-      //     wrongAns++;
-      //   }else if (result[i].round_q1_p1 == 14) {
-      //     noAns++;
-      //   }
-      }
-      // console.log('V : ', rightAns, 'W : ', wrongAns, 'N : ', noAns);
-
-
-  );
-
-});
+}); //end router.get /
 
 router.get('/modify', function(req, res, next) {
-  res.render('modify-profile', {id: req.session.user_id, login: req.session.user_login, friends: req.session.friends})
+  res.render('modify-profile', {
+    id: req.session.user_id,
+    login: req.session.user_login,
+    friends: req.session.friends
+  })
 });
 
 
